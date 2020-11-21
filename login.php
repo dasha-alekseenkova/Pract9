4194988198dece459b310772d5ce487e4b85f5aa
dasha-alekseenkova <72573782+dasha-alekseenkova@users.noreply.github.com>
2020-11-21 09:17:49 +0200

    Update login.php

--- login.php
+++ login.php
@@ -1,1 +1,43 @@
+<!DOCTYPE html>
+<html>
+<body>
+<form action="/" method="post">
+ Логин: <input type="text" name="login" />
 
+ Пароль: <input type="password" name="password" />
+ <input type="submit" value="Войти" name="log_in" />
+
+ </form>
+<?
+include ('lib/connect.php'); 
+include ('lib/module_global.php'); 
+
+ 
+if($_GET['action'] == "out") out(); 
+ 
+if (login()) 
+
+{
+	$UID = $_SESSION['id']; 
+	$admin = is_admin($UID); 
+
+}
+else 
+{
+	if(isset($_POST['log_in'])) 
+	{
+		$error = enter();
+
+		if (count($error) == 0) 
+		{
+			$UID = $_SESSION['id'];
+
+			$admin = is_admin($UID);
+		}
+	}
+}
+
+
+?>
+</body>
+</html>
