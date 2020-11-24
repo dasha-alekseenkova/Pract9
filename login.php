95c6d2b8f8f81219a2c02fe3db869116d507b1ac
dasha-alekseenkova <72573782+dasha-alekseenkova@users.noreply.github.com>
2020-11-24 07:15:14 +0200

    Update login.php

--- login.php
+++ login.php
@@ -1,40 +1,27 @@
 <!DOCTYPE html>
 <html>
 <body>
-<form action="/" method="post">
- Логин: <input type="text" name="login" />
-
- Пароль: <input type="password" name="password" />
- <input type="submit" value="Войти" name="log_in" />
-
- </form>
-<?
-
-if($_GET['action'] == "out") out(); 
- 
-if (login()) 
-
+<br /> 
+<form method="post">
+Username: <input type="text" name="user" /> <br /> 
+Password: <input type="password" name="pass" /> <br />
+<input type="submit" name="submit" value="Login" />
+</form>
+<?php
+session_start();
+$users = 'admin';
+$pass = '1111';
+ if($_POST['submit']){
+ if($users == $_POST['user'] AND $pass == md5($_POST['pass']))
 {
-	$UID = $_SESSION['id']; 
-	$admin = is_admin($UID); 
-
-}
+ $_SESSION['admin'] = $users;
+ header("Location: redirected.php");
+ exit;
+ }
 else 
-{
-	if(isset($_POST['log_in'])) 
-	{
-		$error = enter();
-
-		if (count($error) == 0) 
-		{
-			$UID = $_SESSION['id'];
-
-			$admin = is_admin($UID);
-		}
-	}
-}
-
-
+	header("Location: redirected.php");
+ exit;
+} 
 ?>
 </body>
 </html>
